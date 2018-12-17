9483df86bdaa26ebec7b858729a57b6c980b2f80
Fedor Indutny <fedor@indutny.com>
2018-12-17 12:12:15 -0500

    collect: customizable host

--- bin/collect.ts
+++ bin/collect.ts
@@ -9,7 +9,7 @@ import { UserList, KeyList, IPair, IUser } from '../src/common';
 
 const debug = debugAPI('github-scan');
 
-const GITHUB_API = 'https://api.github.com';
+const GITHUB_API = process.env.GITHUB_API || 'https://api.github.com';
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
--- src/common.ts
+++ src/common.ts
@@ -1,7 +1,5 @@
 import { Readable } from 'stream';
 
-const TWO = BigInt(2);
-
 export interface IKey {
   readonly id: number;
   readonly key: string;
@@ -22,8 +20,6 @@ export interface IPair {
   readonly user: IUser;
   readonly keys: KeyList;
 }
-
-export type ProductTree = ReadonlyArray<ReadonlyArray<bigint>>;
 
 export async function* splitParse<T>(stream: Readable,
                                      parse: (v: string) => T) {
@@ -50,48 +46,4 @@ export async function* splitParse<T>(stream: Readable,
   if (buffer) {
     yield parse(buffer);
   }
-}
-
-export function productTree(values: ReadonlyArray<bigint>): ProductTree {
-  if (values.length === 1) {
-    return [ [ values[0] ] ];
-  }
-
-  if (values.length % 2 !== 0) {
-    throw new Error('Invalid values');
-  }
-
-  const res: bigint[] = [];
-  for (let i = 0; i < values.length; i += 2) {
-    const left = values[i];
-    const right = values[i + 1];
-
-    res.push(left * right);
-  }
-
-  return productTree(res).concat([ values ]);
-}
-
-export function remainderTree(tree: ProductTree): ReadonlyArray<bigint> {
-  let prev: bigint[] = tree[0].slice();
-
-  for (let i = 1; i < tree.length; i++) {
-    const curr = tree[i];
-
-    const result: bigint[] = [];
-    for (let j = 0; j < curr.length; j++) {
-      result.push(prev[j >>> 1] % (curr[j] ** TWO));
-    }
-    prev = result;
-  }
-  return prev;
-}
-
-export function gcd(a: bigint, b: bigint): bigint {
-  while (b !== 0n) {
-    const t = a % b;
-    a = b;
-    b = t;
-  }
-  return a;
 }
